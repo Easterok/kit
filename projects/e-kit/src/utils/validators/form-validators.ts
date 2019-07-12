79178f2a4c361c3ebb9ef5165e47e41feff626a6
Konstantin Beskrovniy <beskrovnyk@gmail.com>
2019-07-12 18:21:25 +0300

    add validators add spaces less

--- projects/e-kit/src/utils/validators/form-validators.ts
+++ projects/e-kit/src/utils/validators/form-validators.ts
@@ -4,28 +4,56 @@ const emailRegExp = /^[-\w]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/;
 const usernameRegExp = /^[a-zA-Zа-яА-ЯёЁ][a-zA-Zа-яА-ЯёЁ0-9\-_+@.]{2,30}$/;
 const passwordRegExp = /^(?=.*[A-zА-яёЁ])(?=.*[\d@.+\-])[A-zА-яёЁ\d@.+\-]{8,50}$/;
 
-export function emailValidator(control: AbstractControl): any {
+export class EkitValidatorsError {
+    constructor(private readonly message: string) {}
+
+    toString(): string {
+        return this.message;
+    }
+}
+
+export function emailValidator(
+    control: AbstractControl,
+): {email: EkitValidatorsError} | null {
     return emailRegExp.test(control.value)
-        ? {}
+        ? null
         : {
-              email:
+              email: new EkitValidatorsError(
                   'Почта должна быть в правильном формате (к примеру exapmle@example.com)',
+              ),
           };
 }
 
-export function usernameValidator(control: AbstractControl): any {
+export function usernameValidator(
+    control: AbstractControl,
+): {username: EkitValidatorsError} | null {
     return usernameRegExp.test(control.value)
-        ? {}
+        ? null
         : {
-              username:
+              username: new EkitValidatorsError(
                   'Имя пользователя должно начинаться с буквы и не содержать символы кроме @ - + _ .',
+              ),
           };
 }
 
-export function passwordValidator(control: AbstractControl): any {
+export function passwordValidator(
+    control: AbstractControl,
+): {password: EkitValidatorsError} | null {
     return passwordRegExp.test(control.value)
-        ? {}
-        : {password: 'Пароль должен содержать цифры и буквы и символы @ - + _ .'};
+        ? null
+        : {
+              password: new EkitValidatorsError(
+                  'Пароль должен содержать цифры и буквы и символы @ - + _ .',
+              ),
+          };
+}
+
+export function requiredValidator(
+    control: AbstractControl,
+): {required: EkitValidatorsError} | null {
+    return !!control.value
+        ? null
+        : {required: new EkitValidatorsError('Поле обязательно для заполнения')};
 }
 
 export function matchingPasswords(passwordKey: string, confirmPasswordKey: string): any {
